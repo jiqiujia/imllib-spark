309b9457518bed957fb5f69f7803bd82c8515629
ynXiang <yinan.xiang@intel.com>
2017-02-10 15:38:05 +0800

    Add examples, data, travis, scalastyle, .gitignore
    Signed-off-by: ynXiang <yinan.xiang@intel.com>

--- src/main/scala/com/intel/imllib/fm/regression/FactorizationMachine.scala
+++ src/main/scala/com/intel/imllib/fm/regression/FactorizationMachine.scala
@@ -94,87 +94,10 @@ class FMModel(val task: Int,
   override protected def formatVersion: String = "1.0"
 
   override def save(sc: SparkContext, path: String): Unit = {
-/*
-    val data = FMModel.SaveLoadV1_0.Data(factorMatrix, weightVector, intercept, min, max, task)
-    FMModel.SaveLoadV1_0.save(sc, path, data)
-*/
   }
 }
 
 object FMModel{
-//object FMModel extends Loader[FMModel] {
-/*
-  private object SaveLoadV1_0 {
-    def thisFormatVersion = "1.0"
-
-    def thisClassName = "org.apache.spark.mllib.regression.FMModel"
-
-    /* Model data for model import/export */
-    case class Data(factorMatrix: Matrix, weightVector: Option[Vector], intercept: Double,
-                    min: Double, max: Double, task: Int)
-
-    def save(sc: SparkContext, path: String, data: Data): Unit = {
-      val sqlContext = new SQLContext(sc)
-      import sqlContext.implicits._
-      // Create JSON metadata.
-      val metadata = compact(render(
-        ("class" -> this.getClass.getName) ~ ("version" -> thisFormatVersion) ~
-          ("numFeatures" -> data.factorMatrix.numCols) ~ ("numFactors" -> data.factorMatrix.numRows)
-          ~ ("min" -> data.min) ~ ("max" -> data.max) ~ ("task" -> data.task)))
-      sc.parallelize(Seq(metadata), 1).saveAsTextFile(metadataPath(path))
-
-      // Create Parquet data.
-      val dataRDD: DataFrame = sc.parallelize(Seq(data), 1).toDF()
-      dataRDD.write.parquet(dataPath(path))
-    }
-
-    def load(sc: SparkContext, path: String): FMModel = {
-      val sqlContext = new SQLContext(sc)
-      // Load Parquet data.
-      val dataRDD = sqlContext.parquetFile(dataPath(path))
-      // Check schema explicitly since erasure makes it hard to use match-case for checking.
-      checkSchema[Data](dataRDD.schema)
-      val dataArray = dataRDD.select("task", "factorMatrix", "weightVector", "intercept", "min", "max").take(1)
-      assert(dataArray.length == 1, s"Unable to load FMModel data from: ${dataPath(path)}")
-      val data = dataArray(0)
-      val task = data.getInt(0)
-      val factorMatrix = data.getAs[Matrix](1)
-      val weightVector = data.getAs[Option[Vector]](2)
-      val intercept = data.getDouble(3)
-      val min = data.getDouble(4)
-      val max = data.getDouble(5)
-      new FMModel(task, factorMatrix, weightVector, intercept, min, max)
-    }
-  }
-
-  override def load(sc: SparkContext, path: String): FMModel = {
-    implicit val formats = DefaultFormats
-
-    val (loadedClassName, version, metadata) = loadMetadata(sc, path)
-    val classNameV1_0 = SaveLoadV1_0.thisClassName
-
-    (loadedClassName, version) match {
-      case (className, "1.0") if className == classNameV1_0 =>
-        val numFeatures = (metadata \ "numFeatures").extract[Int]
-        val numFactors = (metadata \ "numFactors").extract[Int]
-        val model = SaveLoadV1_0.load(sc, path)
-        assert(model.factorMatrix.numCols == numFeatures,
-          s"FMModel.load expected $numFeatures features," +
-            s" but factorMatrix had columns of size:" +
-            s" ${model.factorMatrix.numCols}")
-        assert(model.factorMatrix.numRows == numFactors,
-          s"FMModel.load expected $numFactors factors," +
-            s" but factorMatrix had rows of size:" +
-            s" ${model.factorMatrix.numRows}")
-        model
-
-      case _ => throw new Exception(
-        s"FMModel.load did not recognize model with (className, format version):" +
-          s"($loadedClassName, $version).  Supported:\n" +
-          s"  ($classNameV1_0, 1.0)")
-    }
-  }
-*/
 }
 
 /**
---
+++ src/main/scala/example/FFMExample.scala
@@ -0,0 +1,51 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.classification._
+import org.apache.spark.rdd.RDD
+
+import com.intel.imllib.ffm.classification._
+
+object FFMExample extends App {
+
+  override def main(args: Array[String]): Unit = {
+
+    val sc = new SparkContext(new SparkConf().setAppName("FFMExample"))
+    
+    if (args.length != 8) {
+      println("FFMExample <train_file> <k> <n_iters> <eta> <lambda> " + "<normal> <random> <partitions>")
+    }
+
+    val data = sc.textFile(args(0)).map(_.split("\\s")).map(x => {
+      val y = if(x(0).toInt > 0 ) 1.0 else -1.0
+      val nodeArray: Array[FFMNode] = x.drop(1).map(_.split(":")).map(x => {
+        val node = new FFMNode; node.f = x(0).toInt; node.j = x(1).toInt; node.v = x(2).toDouble; node
+      })
+      (y, nodeArray)
+    }).repartition(args(7).toInt)
+
+    val splits = data.randomSplit(Array(0.8, 0.2))
+    val (training, testing) = (splits(0), splits(1))
+
+    val m = training.flatMap(x=>x._2).map(_.f).collect.reduceLeft(_ max _) //+ 1
+    val n = training.flatMap(x=>x._2).map(_.j).collect.reduceLeft(_ max _) //+ 1
+
+    val param: FFMParameter = new FFMParameter().defaultParameter
+    param.k = args(1).toInt
+    param.n_iters = args(2).toInt
+    param.eta = args(3).toDouble
+    param.lambda = args(4).toDouble
+    param.normalization = args(5).toBoolean
+    param.random = args(6).toBoolean
+
+println("dataset feature:" + n + ",field:" + m + ",k:" + param.k + ",stepsize:" + param.eta)
+
+    val ffm: FFMModel = FFMWithAdag.train(training, m, n, param, "adagrad")
+    val scores: RDD[(Double, Double)] = testing.map(x => {
+      val p = ffm.predict(x._2)
+      val ret = if (p >= 0.5) 1.0 else -1.0
+      (ret, x._1)
+    })
+    val accuracy = scores.filter(x => x._1 == x._2).count().toDouble / scores.count()
+    println(s"accuracy = $accuracy")
+  }
+}
+
