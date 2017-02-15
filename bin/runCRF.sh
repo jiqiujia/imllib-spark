4f3e8c4437fb183563c69e4d3582ec57e1f23bbd
ynXiang <yinan.xiang@intel.com>
2017-02-15 16:20:57 +0800

    Revise all run*.sh
    Signed-off-by: ynXiang <yinan.xiang@intel.com>

--- bin/runCRF.sh
+++ bin/runCRF.sh
@@ -1,10 +1,7 @@
 $SPARK_HOME/bin/spark-submit \
     --class CRFFromParsedFileExample \
-    --master spark://sr443:7077 \
-    --total-executor-cores 144 \
-    --executor-memory 180g \
-    --driver-memory 180g \
+    --master local \
     target/scala-2.11/imllib_2.11-0.0.1.jar \
     data/crf/template \
-    hdfs://sr443/data/serialized/train.data \
-    hdfs://sr443/data/serialized/test.data
+    hdfs://localhost:9000/data/serialized/train.data \
+    hdfs://localhost:9000/data/serialized/test.data
--- bin/runFFM.sh
+++ bin/runFFM.sh
@@ -1,15 +1,12 @@
 $SPARK_HOME/bin/spark-submit \
     --class FFMExample \
-    --master spark://sr443:7077 \
-    --total-executor-cores 144 \
-    --executor-memory 180g \
-    --driver-memory 180g \
+    --master local \
     target/scala-2.11/imllib_2.11-0.0.1.jar \
-    hdfs://sr443:8020/data/a9a_ffm \
-    8 \
-    10 \
+    hdfs://localhost:9000/data/a9a_ffm \
+    2 \
+    3 \
     0.01 \
     0.00002 \
     false \
     false \
-    144
+    2
