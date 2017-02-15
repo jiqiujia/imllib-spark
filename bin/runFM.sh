8f2899b757f04cf6f1c133fc14676584ad9e239a
ynXiang <yinan.xiang@intel.com>
2017-02-15 15:53:02 +0800

    Revise runFM
    Signed-off-by: ynXiang <yinan.xiang@intel.com>

--- bin/runFM.sh
+++ bin/runFM.sh
@@ -1,6 +1,6 @@
 $SPARK_HOME/bin/spark-submit \
     --class FMExample \
-    --master spark://localhost:7077 \
+    --master local \
     target/scala-2.11/imllib_2.11-0.0.1.jar \
     hdfs://localhost:9000/data/fm/a9a \
     hdfs://localhost:9000/data/fm/a9a.t \
