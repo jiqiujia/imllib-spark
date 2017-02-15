62660aea9a9a2fb3c3d5460bdf484e6b5cb834a9
ynXiang <yinan.xiang@intel.com>
2017-02-15 14:42:21 +0800

    Revise FM trainFile
    Signed-off-by: ynXiang <yinan.xiang@intel.com>

--- bin/runFM.sh
+++ bin/runFM.sh
@@ -2,8 +2,8 @@ $SPARK_HOME/bin/spark-submit \
     --class FMExample \
     --master local \
     target/scala-2.11/imllib_2.11-0.0.1.jar \
-    data/fm/a9a \
-    data/fm/a9a.t \
+    data/a9a \
+    data/a9a.t \
     144 \
     30 \
     0.01 \
