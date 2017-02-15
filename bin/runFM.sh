7649100c788455bea605d1d3362da254e83549ce
ynXiang <yinan.xiang@intel.com>
2017-02-15 15:37:12 +0800

    Revise travis.yml, runFM.sh
    Signed-off-by: ynXiang <yinan.xiang@intel.com>

--- bin/runFM.sh
+++ bin/runFM.sh
@@ -1,6 +1,6 @@
 $SPARK_HOME/bin/spark-submit \
     --class FMExample \
-    --master local \
+    --master spark://localhost:7077 \
     target/scala-2.11/imllib_2.11-0.0.1.jar \
     data/a9a \
     data/a9a.t \
