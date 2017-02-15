ed329b997d9fe26687fdbd88f7bf6f7631da3c6e
ynXiang <yinan.xiang@intel.com>
2017-02-15 16:39:08 +0800

    Revise runCRF & runFFM. Add license for FFMExample
    Signed-off-by: ynXiang <yinan.xiang@intel.com>

--- bin/runFFM.sh
+++ bin/runFFM.sh
@@ -2,7 +2,7 @@ $SPARK_HOME/bin/spark-submit \
     --class FFMExample \
     --master local \
     target/scala-2.11/imllib_2.11-0.0.1.jar \
-    hdfs://localhost:9000/data/a9a_ffm \
+    hdfs://localhost:9000/data/ffm/a9a_ffm \
     2 \
     3 \
     0.01 \
