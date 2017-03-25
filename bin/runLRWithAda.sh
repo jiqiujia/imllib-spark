1f61d4ac3c8c730669c94e90e322ecf1c332d410
Vincent Xie <wxie6@liangami-mobl1.ccr.corp.intel.com>
2017-03-25 17:52:14 +0800

    [LR] modify the test scripts for LRWithAdaExample
    Signed-off-by: Vincent Xie <wxie6@liangami-mobl1.ccr.corp.intel.com>

--- bin/runLRWithAda.sh
+++ bin/runLRWithAda.sh
@@ -3,7 +3,4 @@ $SPARK_HOME/bin/spark-submit \
     --master local[*] \
     target/scala-2.11/imllib_2.11-0.0.1.jar \
     hdfs://localhost:9000/data/fm/a9a \
-    2 \
-    3 \
-    0.01 \
-    2
+    hdfs://localhost:9000/data/fm/a9a.t \
